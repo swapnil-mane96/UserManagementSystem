0589207f14193dedbcaaa780dec4f5e4e5f74861
Swapnil Mane <maneswap1@gmail.com>
2023-12-18 23:38:33 +0530

    added update user details feature

--- UserManagementSystem/src/main/java/com/user/controller/UsersController.java
+++ UserManagementSystem/src/main/java/com/user/controller/UsersController.java
@@ -1,14 +1,22 @@
 package com.user.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.user.dtos.UsersDto;
+import com.user.entity.Users;
 import com.user.model.ReturnData;
 import com.user.service.UserService;
 
@@ -27,7 +35,30 @@ public class UsersController {
 		returnData.setMessage("User added successfully...");
 		returnData.setStatusCode("200");
 		return new ResponseEntity<ReturnData>(returnData, HttpStatus.OK);
-		
+	}
+	
+	@GetMapping("/getallusers")
+	public ResponseEntity<?> getAllUsers(){
+		ReturnData returnData = new ReturnData();
+		List<Users> allUsers = this.userService.getAllUsers();
+		returnData.setData(allUsers);
+		return new ResponseEntity<ReturnData>(returnData, HttpStatus.OK);
+	}
+	
+	@GetMapping("/getoneuser/{userid}")
+	public ResponseEntity<?> getOneUser(@PathVariable Integer userid){
+		UsersDto oneUser = this.userService.getOneUser(userid);
+		return new ResponseEntity<UsersDto>(oneUser, HttpStatus.OK);
+	}
+	
+	@PutMapping("/updateuser/{userId}")
+	public ResponseEntity<?> updateUserData(@PathVariable Integer userId, @RequestBody UsersDto usersDto){
+		ReturnData returnData = new ReturnData();
+		UsersDto updateUserData = this.userService.updateUserData(userId, usersDto);
+		returnData.setData(updateUserData);
+		returnData.setMessage("User details are updated successfully");
+		returnData.setStatusCode("200");
+		return new ResponseEntity<ReturnData>(returnData, HttpStatus.OK);
 	}
 
 }
--- UserManagementSystem/src/main/java/com/user/service/UserService.java
+++ UserManagementSystem/src/main/java/com/user/service/UserService.java
@@ -1,7 +1,22 @@
 package com.user.service;
 
+import java.util.List;
+
 import com.user.dtos.UsersDto;
+import com.user.entity.Users;
 
 public interface UserService {
+	// used to add new user
 	UsersDto addNewUser(UsersDto usersDto);
+	
+	//used to get all users list
+	List<Users> getAllUsers();
+	
+	// get single user
+	UsersDto getOneUser(Integer id);
+	
+	// update user data
+	UsersDto updateUserData(Integer userId, UsersDto usersDto);
+	
+	
 }
